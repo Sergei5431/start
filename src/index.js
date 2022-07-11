99111e4a6aa1236e1f22ad8debabe6d0c033e43e
sergei5431-JSFEPRESCHOOL <ykishenii@gmail.com>
2022-07-11 14:47:56 +0300

    qqqq

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ class Card {
     setCard() {
         for (let i = 0; i < arr.length; i++) {
             let card = document.createElement('div');
-            card.classList.add('card-phone')
+            card.classList.add('card-phone');
+            card.setAttribute('data-number', `${i+1}`)
+            card.setAttribute('data-letter', `${i}`)
             card.insertAdjacentHTML('afterbegin',
                 `
             <h4 class="text-h4">${arr[i].name}</h4>
@@ -32,34 +34,161 @@ allCard.setCard()
 
 const containerCard = document.querySelector('.container-card')
 const cart = document.querySelector('.cart-number')
-let cartNum = 0
 
+// --------------------------------------------------------------
+let readAmounsPhone = localStorage.getItem('amoundPhone')
+let cartNum 
+
+if(typeof readAmounsPhone==='object'){
+    cartNum= 0
+   
+}else{
+    cartNum= (JSON.parse(readAmounsPhone))
+}
+// ---------------------------------------------------------------
 const cardDivs = document.querySelectorAll('.card-phone')
 console.log(cardDivs)
-
+// ---------------------------------------------------------------------
+let readLoc = localStorage.getItem('arrLocalRibbon')
+console.log(readLoc)
+console.log(typeof readLoc)
+// let arrLocalRibbon1=[]
+let arrLocalRibbon
+if(typeof readLoc==='object'){
+     arrLocalRibbon= []
+    }else{
+     arrLocalRibbon= [].concat(JSON.parse(readLoc))
+    }
+// ------------------------------------------------------------
+console.log(arrLocalRibbon)
 const whereClick = (e)=>{
     if (e.currentTarget.className == 'card-phone') {
         if (e.currentTarget.querySelector('.ribbon')) {
             e.currentTarget.querySelector('.ribbon').classList.remove('ribbon')
+            let localRibbon = e.currentTarget.dataset.number
+            arrLocalRibbon.splice(arrLocalRibbon.indexOf(+localRibbon),1)
+console.log(localRibbon)
+            localStorage.setItem('arrLocalRibbon', JSON.stringify(arrLocalRibbon))
             cartNum--
             localStorage.setItem('amoundPhone', JSON.stringify(cartNum))
             let local = localStorage.getItem('amoundPhone')
+            
             cart.innerHTML = +local
-                    } else {
+            console.log(arrLocalRibbon)
+            } else {
             let ribbon = document.createElement('div');
             ribbon.classList.add('ribbon')
             e.currentTarget.append(ribbon)
+            // let localRibbon = localStorage.setItem('localRibbon', JSON.stringify(e.currentTarget))
+            let localRibbon = e.currentTarget.dataset.number
+            arrLocalRibbon.push(+localRibbon)
+            localStorage.setItem('arrLocalRibbon', JSON.stringify(arrLocalRibbon))
             cartNum++
             localStorage.setItem('amoundPhone', JSON.stringify(cartNum))
             let local = localStorage.getItem('amoundPhone')
             cart.innerHTML = +local
+            console.log(localStorage.getItem('arrLocalRibbon'))
         }
     }
 }
 cardDivs.forEach(el =>{el.addEventListener('click', whereClick)})
 
-console.log(localStorage.getItem('amoundPhone'))
+console.log(localStorage.getItem('arrLocalRibbon'))
 
-let q = localStorage.getItem('amoundPhone')
-cart.innerHTML = +q
+let amoundPhone = localStorage.getItem('amoundPhone')
+cart.innerHTML = +amoundPhone
+
+// ---------------------------------------------------------------
+let readArrLocalRibbon = localStorage.getItem('arrLocalRibbon')
+// console.log(readArrLocalRibbon.length)
+console.log(readArrLocalRibbon)
+console.log(JSON.parse(readArrLocalRibbon))
+// let element = document.querySelector(`[data-number=${localRibbon}]`)
+// let element = document.querySelector(`[data-number='2']`)
+// console.log(element)
+function addRibbon(e){
+    // console.log(e.length)
+    console.log(typeof e)
+    // if(typeof e !=='object'){
+        for(let i=0; i<e.length;i++){
+            let ribbon = document.createElement('div');
+            ribbon.classList.add('ribbon')
+            document.querySelector(`[data-number='${e[i]}']`).append(ribbon)
+        // }
+    }
+        // let ribbon = document.createElement('div');
+    // ribbon.classList.add('ribbon')
+    // e.append(ribbon)
+}
+
+addRibbon(JSON.parse(readArrLocalRibbon))
+
+// localStorage.clear()
+
+// localStorage.clear()
+// cart.innerHTML = JSON.parse(readArrLocalRibbon).length
+// -----------------------------------------------------------------
+// кнопка сброс настроек 
+const resetOptions = document.querySelector('#options')
+// const resetOptions1 = document.querySelector('.card-phone')
+console.log(resetOptions);
+// console.log(resetOptions1)
+const deleteOptions = ()=>{
+    localStorage.removeItem('arrLocalRibbon')
+    localStorage.removeItem('amoundPhone')
+    document.location.reload();
+
+}
+resetOptions.addEventListener('click', deleteOptions)
+// ------------------------------------------------------------------
+const sortingA = document.querySelector('.sortingA')
+const sortingZ = document.querySelector('.sortingZ')
+
+const sortA =()=>{
+    let nav = document.querySelector('.container-card')
+    for(let i =0; i<nav.children.length;i++){
+        for(let j = i; j<nav.children.length;j++){
+            if(+nav.children[i].getAttribute('data-letter') < +nav.children[j].getAttribute('data-letter')){
+                let replaceNode = nav.replaceChild(nav.children[j], nav.children[i])
+                insertAfter(replaceNode, nav.children[i])
+                            }
+        }
+    }
+}
+
+const sortZ =()=>{
+    let nav = document.querySelector('.container-card')
+    for(let i =0; i<nav.children.length;i++){
+        for(let j = i; j<nav.children.length;j++){
+            if(+nav.children[i].getAttribute('data-letter') > +nav.children[j].getAttribute('data-letter')){
+                               let replaceNode = nav.replaceChild(nav.children[j], nav.children[i])
+                insertAfter(replaceNode, nav.children[i])
+            }
+        }
+    }
+}
+
+function insertAfter(elem, refElem){
+    return refElem.parentNode.insertBefore(elem, refElem.nextSibling)
+}
+
+sortingA.addEventListener('click', sortA)
+sortingZ.addEventListener('click', sortZ)
+
+// ----------------------------------------------------------------------
+
+const btnColorRed = document.querySelector('.color-red')
+
+
+
+const showRed = () =>{
+    if(btnColorRed.innerHTML.length==0){
+        btnColorRed.innerHTML = '&#10004'
+    }else{
+        btnColorRed.innerHTML =''
+    }
+    
+}
+
+btnColorRed.addEventListener('click', showRed)
 
